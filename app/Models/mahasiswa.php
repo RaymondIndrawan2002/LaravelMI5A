7e41a42a67dd0c4388d70d754f2be98f8ee18b6f
RaymondIndrawan2002 <raymondindrawansilalahi_2225110002@mhs.mdp.ac.id>
2024-10-25 14:25:00 +0700

    testing
    testing

--- app/Models/mahasiswa.php
+++ app/Models/mahasiswa.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class mahasiswa extends Model
 {
-    use HasFactory, HasUlids;
+    use HasFactory ;
 
     protected $fillable = ["npm","nama","tanggal_lahir","tempat_lahir","email","hp","alamat","prodi_id"];
 
--- app/Models/prodi.php
+++ app/Models/prodi.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class prodi extends Model
 {
-    use HasFactory, HasUlids;
+    use HasFactory ;
 
     protected $fillable = ["nama","kaprodi","singkatan","fakultas_id"];
 
