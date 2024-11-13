69b59ccde73144d718ebef1a90b553d25bd9027e
RaymondIndrawan2002 <raymondindrawansilalahi_22251100002@mhs.mdp.ac.id>
2024-11-13 15:07:29 +0700

    Pemograman Web 2 pertemuan 17
    Pemograman Web 2 pertemuan 17

---
+++ app/Http/Controllers/AuthContoller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthContoller extends Controller
+{
+    public function login(Request $request){
+        if(Auth::attempt([
+        'email' => $request->email,
+        'password' => $request->password
+    ])) {
+        $user = Auth::user();
+        $success['token'] = $user->createToken('MDPApp')->plainTextToken;
+        $success['name'] = $user->name;
+        return response()->json($success, 201);
+    } else {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthContoller;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\MahasiswaController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,12 +12,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('fakultas', [FakultasController::class, 'getFakultas']);
+Route::get('fakultas', [FakultasController::class, 'getFakultas'])->middleware('auth:sanctum');
 
-Route::get('prodi', [ProdiController::class, 'getProdi']);
-Route::get('mahasiswa', [MahasiswaController::class, 'getMahasiswa']);
+Route::get('prodi', [ProdiController::class, 'getProdi'])->middleware('auth:sanctum');
+Route::get('mahasiswa', [MahasiswaController::class, 'getMahasiswa'])->middleware('auth:sanctum');;
 
 Route::post('fakultas', [FakultasController::class, 'storeFakultas']);
 Route::post('prodi', [ProdiController::class, 'storeProdi']);
 
 Route::delete('fakultas/{id}',[FakultasController::class,'destroyFakultas']);
+
+Route::post('login', [AuthContoller::class, 'login']);
